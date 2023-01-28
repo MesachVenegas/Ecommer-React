0cf26b5c3fa1bd18302b4e3fda10a5967842f529
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-27 20:12:47 -0800

    add: function to consult API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import { useState } from 'react'
 import './App.css'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react';
 
 function App() {
+    // https://e-commerce-api-v2.academlo.tech/api/v1/products
+
+    useEffect( () =>{
+        axios.get('https://e-commerce-api-v2.academlo.tech/api/v1/products')
+        .then()
+    },[])
+
 
     return (
         <div className="App">
