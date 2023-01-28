64f82379bb536f59916d2309ec82095658afbe58
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-27 21:40:30 -0800

    add: import bootstrap theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
+import React from 'react';
 import store from './store';
 import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom/client';
+import "bootswatch/dist/morph/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
