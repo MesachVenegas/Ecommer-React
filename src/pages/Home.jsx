c8c4de47657497c64501b55ee3c955add0dc4563
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-28 18:50:34 -0800

    delete: navigation bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import NavigationBar from '../components/NavigationBar';
 
 const Home = () => {
     return (
         <>
-            <NavigationBar />
+            <h1>Home</h1>
         </>
     );
 };
