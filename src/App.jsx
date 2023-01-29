63ae4153eb23cf78c292f8748e5825b9207951fa
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-28 18:50:25 -0800

    add: Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { HashRouter,Routes, Route } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar'
+import Purchases from './pages/Purchases';
 import Product from './pages/Product';
 import Loggin from './pages/Loggin';
 import Home from './pages/Home';
@@ -8,10 +10,12 @@ function App() {
 
     return (
         <HashRouter>
+            <NavigationBar/>
             <Routes>
                 <Route path='/' element={ <Home /> } />
                 <Route path='/products/:id' element={ <Product /> } />
                 <Route path='/loggin' element={ <Loggin /> } />
+                <Route path='/purchases' element={ <Purchases />} />
             </Routes>
         </HashRouter>
     )
