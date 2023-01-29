9efb41364848aace621888277378d55de42c7669
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-28 20:09:15 -0800

    change: renderizado controlado

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     return (
         <HashRouter>
             <NavigationBar/>
-            {isLoading ? <Loader /> : 'none'}
+            {isLoading && <Loader />}
             <Routes>
                 <Route path='/' element={ <Home /> } />
                 <Route path='/products/:id' element={ <Product /> } />
