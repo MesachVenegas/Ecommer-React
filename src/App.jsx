487f2a5ae8bf77506fdb8f1497f1759dda9d0090
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-28 20:06:21 -0800

    add; loader controled

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { HashRouter,Routes, Route } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar'
+import Loader from './components/Loader/Loader';
 import Purchases from './pages/Purchases';
+import { useSelector } from 'react-redux';
 import Product from './pages/Product';
 import Loggin from './pages/Loggin';
 import Home from './pages/Home';
 import './App.css'
 
 function App() {
+    const isLoading = useSelector(state => state.isLoading);
 
     return (
         <HashRouter>
             <NavigationBar/>
+            {isLoading ? <Loader /> : 'none'}
             <Routes>
                 <Route path='/' element={ <Home /> } />
                 <Route path='/products/:id' element={ <Product /> } />
