ad3a7c7aab69b720f7bd33cc28664fc19e2a8bb3
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-28 18:50:16 -0800

    add: navigation link

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,12 +2,14 @@ import Container from 'react-bootstrap/Container';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 
 function NavigationBar() {
+
     return (
         <Navbar bg="dark" variant='dark' expand="sm" className="mb-3" sticky="top">
             <Container fluid>
-                <Navbar.Brand href="/#/">M&K Shop</Navbar.Brand>
+                <Navbar.Brand as={ Link } to='/'>M&K Shop</Navbar.Brand>
                 <Navbar.Toggle aria-controls="offcanvasNavbar-expand-sm" />
                 <Navbar.Offcanvas
                     id= "offcanvasNavbar-expand-sm"
@@ -21,14 +23,14 @@ function NavigationBar() {
                     </Offcanvas.Header>
                     <Offcanvas.Body >
                         <Nav className="justify-content-end flex-grow-1 pe-4">
-                            <Nav.Link href="#!">
-                                <i class="fa-solid fa-user"></i> Loggin
+                            <Nav.Link as={ Link } to='/loggin'>
+                                <i className="fa-solid fa-user"></i> Loggin
                             </Nav.Link>
-                            <Nav.Link href="#!">
-                                <i class="fa-solid fa-shop"></i> Puncharses
+                            <Nav.Link as={ Link } to='/purchases'>
+                                <i className="fa-solid fa-shop"></i> Puncharses
                             </Nav.Link>
-                            <Nav.Link href="#!">
-                                <i class="fa-solid fa-cart-shopping"></i> Cart
+                            <Nav.Link>
+                                <i className="fa-solid fa-cart-shopping"></i> Cart
                             </Nav.Link>
                         </Nav>
                     </Offcanvas.Body>
