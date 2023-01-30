f2096655ce8aba37f65774196643a4f76f61cd01
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-30 05:01:31 -0800

    change: styles

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -3,37 +3,31 @@ import { useNavigate } from 'react-router-dom';
 import Card  from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const CardItem = ({product}) => {
     const navigate = useNavigate()
 
     return (
         <Col>
-            <Card
-                style={
-                    {
-                        width: '20rem',
-                        cursor: 'pointer'
-                    }
-                }
-                onClick={() => navigate(`/products/${product.id}`)}
-            >
+            <Card style={{width: '20rem',cursor: 'pointer'}} onClick={() => navigate(`/products/${product.id}`)}>
                 <Card.Img
                     variant="top"
                     src={product.images[0].url}
                     style={ { height: '200px', objectFit: 'contain' } }
                 />
-                <Card.Footer>
-                    <small className="text-muted">{product.category.name}</small>
-                </Card.Footer>
+                <Container className='mt-2' fluid>
+                    <small className="text-muted">{product.brand}</small>
+                </Container>
                 <Card.Body>
                     <Card.Title>{product.title}</Card.Title>
-                    <Card.Text>
-                        This is a longer card with supporting text below as a natural
-                        lead-in to additional content. This content is a little bit
-                        longer.
+                    <small className="text-muted">Price</small>
+                    <Card.Text className='d-flex justify-content-between align-items-center'>
+                        ${product.price}
+                        <Button className='rounded-circle'>
+                            Buy
+                        </Button>
                     </Card.Text>
-                    <Button>Buy</Button>
                 </Card.Body>
             </Card>
         </Col>
