d9e599042609c08c6a69ca4273f1c362cfda4834
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-30 04:37:56 -0800

    add: search bar and fuction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import CardItem from '../components/CardItem'
 import { useDispatch, useSelector } from "react-redux";
-import { getProductsThunk, filterByCategoryThunk } from "../store/slices/products.slice";
 import { Button, Container, Form, InputGroup, Row } from "react-bootstrap";
-import axios from "axios";
+import { getProductsThunk, filterByCategoryThunk, filterByTitleThunk } from "../store/slices/products.slice";
 
 const Home = () => {
     const dispatch = useDispatch();
     const products = useSelector(state => state.products)
+    const [searchTitle, setSearchTitle] = useState('')
     const [categories, setCategories] = useState([])
 
     useEffect(() => {
@@ -17,38 +18,60 @@ const Home = () => {
     }, [])
 
     return (
-        <Container fluid>
+        <Container className="align-items-center" fluid>
+            {/* Search bar */}
             <Container className="p-2">
                 <InputGroup className="mb-3">
                     <Form.Control
                         placeholder="Name or Id"
                         aria-label="Recipient's username"
                         aria-describedby="basic-addon2"
+                        value={ searchTitle }
+                        onChange={ e => setSearchTitle(e.target.value)}
                     />
-                    <Button variant="outline-secondary" id="button-addon2">
+                    <Button
+                        variant="outline-secondary"
+                        id="button-addon2"
+                        onClick={ () => dispatch(filterByTitleThunk(searchTitle)) }
+                    >
                         Search
                     </Button>
                 </InputGroup>
             </Container>
-            <ul className="categories">
-                <li onClick={ () => dispatch(getProductsThunk()) }>
-                    <a href="#">All</a>
-                </li>
-                {
-                    categories.map( category => (
-                        <li key={category.id} onClick={ () => dispatch(filterByCategoryThunk(category.id))}>
-                            <a href="#">{category.name}</a>
-                        </li>
-                    ))
-                }
-            </ul>
-            <Row xs={1} sm={1} md={2} lg={3} className="g-4">
-                {
-                    products.map(product =>(
-                        <CardItem key={product.id} product={product}/>
-                    ))
-                }
-            </Row>
+            {/* Filter by Category */}
+            <Container>
+                <ul className="categories list-group">
+                    <li
+                        className="list-group-item"
+                        onClick={ () => dispatch(getProductsThunk()) }
+                        style={{ cursor: 'pointer'}}
+                    >
+                        All
+                    </li>
+                    {
+                        categories.map( category => (
+                            <li
+                                className="list-group-item"
+                                key={category.id}
+                                onClick={ () => dispatch(filterByCategoryThunk(category.id))}
+                                style={{ cursor: 'pointer' }}
+                            >
+                                {category.name}
+                            </li>
+                        ))
+                    }
+                </ul>
+            </Container>
+            {/* Listed products availables */}
+            <Container>
+                <Row xs={1} sm={1} md={2} lg={3} className="g-4">
+                    {
+                        products.map(product =>(
+                            <CardItem key={product.id} product={product}/>
+                        ))
+                    }
+                </Row>
+            </Container>
         </Container>
     );
 };
