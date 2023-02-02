1890685feaaf45755678e87fd488384e885d82b5
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-02-01 20:43:56 -0800

    change: adjust product view detail layout

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -35,44 +35,64 @@ const Product = () => {
     },[id])
 
     return (
-        <div className='product_grid'>
-            <div className='breadcrumb'>
+        <div className='product_layout'>
+            <div className='breadcrumb_container'>
                 <Breadcrumb.Item as={ ListGroupItem } onClick={() => navigate('/')}>Home</Breadcrumb.Item>
                 <Breadcrumb.Item as={ListGroupItem} active>{product.brand}</Breadcrumb.Item>
             </div>
-            <figure className='img_container image'>
-                <img src={product.images?.[0].url} alt="" className='product_img'/>
-            </figure>
-            <div className="description content p-5">
-                <small>{product.brand}</small>
-                <h3>{product.title}</h3>
-                <div className='d-flex flex-column'>
-                    <p>
-                        {product.description}
-                    </p>
-                    <div className="d-flex justify-content-around buttons py-4">
-                        <div className="d-flex flex-column price">
-                            <small >Price</small>
-                            <span className='price_prod text-danger'>${product.price}</span>
-                        </div>
-                        <div className="d-flex flex-column price">
-                            <small>Qty</small>
-                            <div className="qty_control">
-                                <button className='btn' onClick={decrement}>
-                                    -
-                                </button>
-                                <span>{qty}</span>
-                                <button className='btn' onClick={increment}>
-                                    +
-                                </button>
+            <div className="item_container">
+                {/* images product */}
+                <div className="slider_product">
+                    <figure className='img_container'>
+                        <span>
+                            <i className="fa-solid fa-circle-chevron-left"></i>
+                        </span>
+                        <img src={product.images?.[0].url} alt="" className='product_img'/>
+                        <span>
+                            <i className="fa-solid fa-circle-chevron-right"></i>
+                        </span>
+                    </figure>
+                    <ul className='images_slider'>
+                        {
+                            product?.images?.map(imagen =>(
+                                <li key={imagen.id} className='product_thumbail'>
+                                    <img src={imagen.url} alt='preview_thumbail' />
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div className="description content p-5">
+                    <small>{product.brand}</small>
+                    <h3>{product.title}</h3>
+                    <div className='d-flex flex-column'>
+                        <p>
+                            {product.description}
+                        </p>
+                        <div className="d-flex justify-content-around buttons py-4">
+                            <div className="d-flex flex-column price">
+                                <small >Price</small>
+                                <span className='price_prod text-danger'>${product.price}</span>
+                            </div>
+                            <div className="d-flex flex-column price">
+                                <small>Qty</small>
+                                <div className="qty_control">
+                                    <button className='btn' onClick={decrement}>
+                                        -
+                                    </button>
+                                    <span>{qty}</span>
+                                    <button className='btn' onClick={increment}>
+                                        +
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="buy_btn d-flex justify-content-center">
-                        <Button className='d-flex bg-danger text-white gap-2 align-items-center' size='lg'>
-                            Add to cart
-                            <i className="fa-solid fa-cart-arrow-down"></i>
-                        </Button>
+                        <div className="buy_btn d-flex justify-content-center">
+                            <Button className='d-flex bg-danger text-white gap-2 align-items-center' size='lg'>
+                                Add to cart
+                                <i className="fa-solid fa-cart-arrow-down"></i>
+                            </Button>
+                        </div>
                     </div>
                 </div>
             </div>
