de814e09beb10fabd4f01495c6261321344e442e
MesachVenegas <mesach.venegas@hotmail.com>
2023-07-05 22:25:57 -0700

    update: Readme

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import './footer.css'
 const Footer = () => {
     return (
         <footer className="footer">
-            <h6>Academlo Gen-21 React Module</h6>
             <p>Made by Mesach Venegas with <i className="fa-solid fa-heart heart"></i></p>
+            <p>Powerwed By Netlify</p>
         </footer>
     );
 };
